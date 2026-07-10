4579155b7d909fc2d5451b6e91ba06240b906f80
Asad1ama <asad.ama339@gmail.com>
2026-07-11 00:26:55 +0530

    day03

--- 01_JavaScript/Day_02/strings.js
+++ 01_JavaScript/Day_02/strings.js
@@ -4,13 +4,25 @@ const repocount=50
 console.log(name + repocount +" value");
 console.log(`Hello my name is ${name} and my repocount is ${repocount}.`);
 
-const gamename=new String('asad')
+const gamename=new String('asad-am-ahsan')
 console.log(gamename[0]);
 console.log(gamename.__proto__);
 console.log(gamename.length);
 console.log(gamename.toUpperCase());
 console.log(gamename.charAt(2));
 console.log(gamename.indexOf('s'));
+
+const newstring=gamename.substring(0,3)
+console.log(newstring);
+
+const anotherstring=gamename.slice(-3,2)
+console.log(anotherstring);
+
+const url ="https://asad%20.com"
+console.log(url.replace('%20','-'))
+console.log(url.includes('anas'))
+console.log(gamename.split('-'));
+
 
 
 
---
+++ 01_JavaScript/Day_03/data-time.js
@@ -0,0 +1,23 @@
+////dates
+
+let mydate= new Date()
+console.log(mydate.toString());
+console.log(mydate.toLocaleDateString());
+console.log(mydate.toLocaleTimeString());
+console.log(mydate.toDateString());
+console.log(mydate.toLocaleString());
+console.log(typeof mydate);
+
+
+let mycreateddate=new Date(2026,6,10)
+console.log(mycreateddate.toDateString());
+
+
+let newDate =new Date()
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay());
+
+
+
+    
